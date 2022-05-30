1bac401c059edba4e7767d484fcb902632549822
Khusan <khu10735@naver.com>
2022-05-30 16:54:28 +0900

    layout

---
+++ src/Fruits.js
@@ -0,0 +1,39 @@
+import React from "react";
+import './Fruits.css'
+
+
+function Fruits(){
+    return(
+        <div className="main">
+          <div className="rasm">
+            
+          </div>
+          <div className="malumotlar">
+            <div className="top">
+                <h1>Celery Stalk</h1>
+                <button className="button">ADD</button>
+            </div>
+            <div className="bottom">
+                <div className="data">
+                    <p className="title">UOM</p>
+                    <p className="value">02</p>
+                </div>
+                <div className="data">
+                    <p className="title">Pack Size</p>
+                    <p className="value">02</p>
+                </div>
+                <div className="data">
+                    <p className="title">Per Unit</p>
+                    <p className="value">02</p>
+                </div>
+                <div className="data">
+                    <p className="title">Total</p>
+                    <p className="value">02</p>
+                </div>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default Fruits
